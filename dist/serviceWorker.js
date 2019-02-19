13daa64dab434b75c2dccc78ab51028f50171f4e
Vegar Ringdal <vegar.ringdal@gmail.com>
2019-02-19 23:38:59 +0100

    update

--- dist/serviceWorker.js
+++ dist/serviceWorker.js
@@ -1,24 +1,24 @@
 (function(){
-	var _d73d = {};
-	_d73d.f = {};
+	var _5753 = {};
+	_5753.f = {};
 	// cached modules
-	_d73d.m = {};
-	_d73d.r = function(id) {
-		var cached = _d73d.m[id];
+	_5753.m = {};
+	_5753.r = function(id) {
+		var cached = _5753.m[id];
 		// resolve if in cache
 		if (cached) {
 			return cached.m.exports;
 		}
-		var file = _d73d.f[id];
+		var file = _5753.f[id];
 		if (!file) return;
-		cached = _d73d.m[id] = {};
+		cached = _5753.m[id] = {};
 		cached.exports = {};
 		cached.m = { exports: cached.exports };
 		file.call(cached.exports, cached.m, cached.exports);
 		return cached.m.exports;
 	};
 // default/worker.js
-_d73d.f[0] =
+_5753.f[0] =
 function(){
 self.addEventListener('install', function (_event) {
 });
@@ -43,6 +43,6 @@ self.addEventListener('fetch', function (event) {
     }));
 });
 }
-_d73d.r(0)
+_5753.r(0)
 })();
 
