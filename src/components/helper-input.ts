7e7b8446339ec17188f308f212c9ccc7e0530a01
Vegar Ringdal <vegar.ringdal@gmail.com>
2019-07-14 21:33:51 +0200

    fix lint errors

--- src/components/helper-input.ts
+++ src/components/helper-input.ts
@@ -10,7 +10,7 @@ export const textInput = (s: {
 return html`
 <div class="flex flex-col m-3 ">
     <label class="mr-2 w-full text-xs font-medium text-gray-600"><u>${s.label}</u></label>
-    <input class="w-full border border-gray-100" .value=${<string>s.defaultvalue} type=${s.type} placeholder=${s.placeholder} 
-        @input=${(e: {target: HTMLInputElement}) => s.callback(s.type === 'number' ? e.target.valueAsNumber: e.target.value)}>
+    <input class="w-full border border-gray-100" .value=${<string>s.defaultvalue} type=${s.type} placeholder=${s.placeholder}
+        @input=${(e: {target: HTMLInputElement}) => s.callback(s.type === 'number' ? e.target.valueAsNumber : e.target.value)}>
 </div>`;
 };
